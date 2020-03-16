745a76ba74d7073e0c6352f5ed379e15a5552144
Jun Jo <junjo@mpi-sws.org>
2020-03-16 15:41:48 +0100

    tests: add rdma related

---
+++ tests/rdma_client.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include <tas_rdma.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+
+int main()
+{
+  const char ip[] = "10.0.0.2";
+  rdma_init();
+  struct sockaddr_in remoteaddr;
+  remoteaddr.sin_family = AF_INET;
+  remoteaddr.sin_addr.s_addr = inet_addr(ip);
+  remoteaddr.sin_port = htons(5005);
+
+  void *mr_base;
+  uint32_t mr_len;
+
+  int fd = rdma_connect(&remoteaddr, &mr_base, &mr_len);
+
+  if (fd < 0)
+    fprintf(stderr, "Connection failed\n");
+
+  getchar();
+
+  char *ptr = (char*) mr_base;
+  for (int i = 0; i < 512; i++)
+  {
+    fprintf(stderr, "%c", ptr[i]);
+  }
+  fprintf(stderr, "\n");
+
+  return 0;
+}
--- tests/rules.mk
+++ tests/rules.mk
@@ -30,8 +30,15 @@ TESTS_AUTO := \
 TESTS_AUTO_FULL := \
   tests/full/tas_linux \
 
+# rdma tests
+TESTS_RDMA := \
+  tests/rdma_client \
+  tests/rdma_server \
+  tests/rdma_multi_client \
+  tests/rdma_multi_server \
+
 TESTS := $(TESTS_NONE) $(TESTS_LIBTAS) $(TESTS_SOCKETS) $(TESTS_AUTO) \
-  $(TESTS_AUTO_FULL)
+  $(TESTS_AUTO_FULL) $(TESTS_RDMA)
 TEST_OBJS := $(addsuffix .o, $(TESTS)) \
   tests/testutils.o tests/libtas/harness.o tests/full/fulltest.o
 
@@ -43,6 +50,9 @@ $(foreach t,$(TESTS_LIBTAS),$(eval $(t): $(t).o lib/libtas.so))
 $(TESTS_SOCKETS): CPPFLAGS += -Ilib/sockets/include/
 $(foreach t,$(TESTS_SOCKETS),$(eval $(t): $(t).o lib/libtas_sockets.so))
 
+# tests linking against librdma
+$(TESTS_RDMA): CPPFLAGS += -Ilib/rdma/include/
+$(foreach t,$(TESTS_RDMA),$(eval $(t): $(t).o lib/libtas_rdma.so))
 
 tests/libtas/tas_ll: CPPFLAGS += -Ilib/tas/include/
 tests/libtas/tas_ll: tests/libtas/tas_ll.o tests/libtas/harness.o \
@@ -57,13 +67,16 @@ tests/tas_unit/fastpath: CFLAGS+= $(DPDK_CFLAGS)
 tests/tas_unit/fastpath: LDFLAGS+= $(DPDK_LDFLAGS)
 tests/tas_unit/fastpath: LDLIBS+= -lrte_eal
 tests/tas_unit/fastpath: tests/tas_unit/fastpath.o tests/testutils.o \
-  tas/fast/fast_flows.o
+  tas/fast/fast_flows.o tas/fast/fast_rdma.o
 
 tests/full/%.o: CPPFLAGS+=-Ilib/tas/include
 tests/full/tas_linux: tests/full/tas_linux.o tests/full/fulltest.o lib/libtas.so
 
 # build tests
 tests: $(TESTS)
+
+# build rdma tests
+tests-rdma: $(TESTS_RDMA)
 
 # run all simple testcases
 run-tests: $(TESTS_AUTO)
